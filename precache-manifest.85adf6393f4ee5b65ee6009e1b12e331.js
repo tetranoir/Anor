7a70f47ca2668d0a39dcbb09b0a935ef18a09fd4
arno <tetranoir@gmail.com>
2019-11-29 18:35:04 -0800

    Updates

---
+++ precache-manifest.85adf6393f4ee5b65ee6009e1b12e331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc47895d8440ef0cc9466a4539650e1",
+    "url": "/anor/index.html"
+  },
+  {
+    "revision": "a43435159bcf67b3e252",
+    "url": "/anor/static/css/main.08efdcc0.chunk.css"
+  },
+  {
+    "revision": "9a0c2b7a844a323e43f5",
+    "url": "/anor/static/js/2.aef2a808.chunk.js"
+  },
+  {
+    "revision": "a43435159bcf67b3e252",
+    "url": "/anor/static/js/main.1af89320.chunk.js"
+  },
+  {
+    "revision": "f491438397a24bb9b9ad",
+    "url": "/anor/static/js/runtime~main.e1f3c5bf.js"
+  }
+]);
